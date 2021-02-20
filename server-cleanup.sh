e8743e7b712b8a85b036f9c64097447974825c55
Guest0x0 <guest0x0@outlook.com>
2021-02-20 15:37:54 +0800

    change comment on iptables rules

--- server-cleanup.sh
+++ server-cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-iptables -t nat -D POSTROUTING -m comment --comment "gfw-udptun" -j MASQUERADE -s 10.0.1.0/24
+iptables -t nat -D POSTROUTING -m comment --comment "ipudp" -j MASQUERADE -s 10.0.1.0/24
--- server.sh
+++ server.sh
@@ -4,4 +4,4 @@ ifconfig $TUN_NAME up 10.0.1.0/24
 
 echo 1 | dd of=/proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -A POSTROUTING -m comment --comment "gfw-udptun" -j MASQUERADE -s 10.0.1.0/24
+iptables -t nat -A POSTROUTING -m comment --comment "ipudp" -j MASQUERADE -s 10.0.1.0/24
